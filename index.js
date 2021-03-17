d153104714bb1f24393dbbfec01364494b7e515f
Azlan <azlan@123>
2021-03-18 03:05:18 +0530

    All View part is done at basic lavel including Layout, partial files and static filed in assets folder

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const app = express();
+const expressLayouts = require('express-ejs-layouts');
 const port = 8000;
 
 const router = express.Router();
 
+app.use(expressLayouts); //Always placed before routes middleware
 app.use('/', require('./routes/index'));
+app.use(express.static('./assets'));
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+// extract style and script from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+
 
 
 app.listen(port, function (err) {
